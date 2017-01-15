9e356770ceb01bc29b99264c53b3dde5a7b06183
seijiFeather <syag.feather@gmail.com>
2017-01-15 20:17:29 +0900

    Goalsの実装 内部に順列のすべての組み合わせを求めるpermutationクラスを実装

--- app/services/Goals.java
+++ app/services/Goals.java
@@ -3,5 +3,137 @@ package services;
 /**
  * Created by seijihagawa on 2017/01/12.
  */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * どの副目標から達成していくのかを管理します。
+ * targetにするblockの処理の順序を管理する
+ * <p>
+ * mIDMap 系列はすべてint型の番号で管理しているのでIDとmappingさせるためのmap
+ */
 public class Goals {
+    private String[] mIDs;
+    private String mCurrentID;
+    private int mNumberInLine;
+    private int mSeriesNumber;
+    private ArrayList<ArrayList<Integer>> mSeriesInteger = new ArrayList<>(); //初期化のタイミングって何か違いはあるのだろうか....
+    private HashMap<Integer, String> mIDMap = new HashMap<>();
+
+    public Goals(String[] aIDs) {
+        mIDs = aIDs;
+        Permutation tPerm = new Permutation();
+        tPerm.permutation(mIDs.length);
+        mSeriesInteger = tPerm.getNumberList();
+
+        for (int i = 1; i <= mIDs.length; i++) {
+            mIDMap.put(i, mIDs[i - 1]);
+        }
+
+        setSeriesNumber();
+        mNumberInLine = 0;
+    }
+
+    /**
+     * どの系列からアプローチするかの決定則が実装されている
+     *
+     * この決定則に自由度を持たせるなら、permutationでの実装は微妙
+     */
+    private void setSeriesNumber() {
+        mSeriesNumber = 0;
+    }
+
+    public String getCurrentID() {
+        return mCurrentID;
+    }
+
+    public void setNextID(){
+        mNumberInLine++;
+        int tNumber = mSeriesInteger.get(mSeriesNumber).get(mNumberInLine);
+        mCurrentID = mIDMap.get(tNumber);
+    }
+
+    /**
+     * 別の系列を呼び出す
+     *
+     * 現在の系列で推論がうまくいかなった場合に読み出される
+     */
+    //系列の決め方ももう少し自由度をつける方が良い
+    public void setNewSeiries(){
+        mSeriesNumber++;
+        mNumberInLine = 0;
+        int tNumber = mSeriesInteger.get(mSeriesNumber).get(mNumberInLine);
+        mCurrentID = mIDMap.get(tNumber);
+    }
+
+    /**
+     *
+     * @return
+     * いるかどうかわからんけど一応。
+     */
+    public String getNextID(){
+        setNextID();
+        return getCurrentID();
+    }
+
+
+    /**
+     * 順列の総数を求めるクラス(util)
+     * <p>
+     * <p>
+     * utilとして外だしすべきかな
+     * 内部実装が汚い
+     * mPermutationListのスコープが広すぎる
+     */
+    private class Permutation {
+        private int mNumber;
+        private ArrayList<ArrayList<Integer>> mNumberList = new ArrayList<>();
+        private ArrayList<Integer> mPermutationList = new ArrayList<>();
+
+        Permutation() {
+        }
+
+        public void permutation(int aNumber) {
+            ArrayList<Integer> tSource = createList(aNumber);
+            arrangeRecursive(1, tSource);
+        }
+
+
+        /**
+         * @param aCounter    再帰した回数を記録する mNumberと等しくなったら終了. 呼び出しは必ず1で初期化
+         * @param aSourceList 再帰での実装はオーバーヘッドが多分大きいので、再帰を使わない実装にしたい
+         */
+        private void arrangeRecursive(int aCounter, ArrayList<Integer> aSourceList) {
+
+            if (aCounter == mNumber) {
+                mNumberList.add(mPermutationList);
+                mPermutationList = new ArrayList<Integer>(); //これ、コンパイルしたらうまくいくんじゃない？
+                return;
+            }
+
+            for (Integer tNum : aSourceList) {
+                ArrayList<Integer> tSourceBuf = new ArrayList<>(aSourceList);
+                mPermutationList.add(tNum);
+                tSourceBuf.remove(tNum);
+                arrangeRecursive(aCounter + 1, tSourceBuf);
+            }
+
+        }
+
+        private ArrayList<Integer> createList(int aNumber) {
+            ArrayList<Integer> tList = new ArrayList<>();
+            for (int i = 1; i <= aNumber; i++) {
+                tList.add(i);
+            }
+
+            return tList;
+        }
+
+        public ArrayList<ArrayList<Integer>> getNumberList() {
+            return mNumberList;
+        }
+
+    }
+
 }
--- app/services/Planner.java
+++ app/services/Planner.java
@@ -3,5 +3,45 @@ package services;
 /**
  * Created by seijihagawa on 2017/01/12.
  */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * 現在の状態が目標状態になっているかを確かめ、プランを適用する
+ *
+ */
+
 public class Planner {
+    private Goals mGoals;
+    private HashMap<String, SubGoals> mSubGoals;
+    private Space mTargetSpace; //これいらないかも
+    private Space[] mTargetOptions;
+    private Space mCurrentSpace;
+    private String mTargetBlockID;   //これグローバル変数として持つ必要ないかもしれない
+    private static  Block[] mBlocks; // このクラスはこの書き方でいいのか？
+
+    Planner(Space[] aTargetOptions, Block[] aBlocks){
+        mTargetOptions = aTargetOptions;
+        mBlocks = aBlocks;
+        mGoals = new Goals();
+        mSubGoals = new HashMap<>();
+        mTargetSpace = new Space();
+        mCurrentSpace = new Space();
+        mTargetBlockID = new String();
+
+    }
+
+
+    /**
+     *
+     * Goalsの系列に対して、その系列をOperationに投げる
+     */
+    public OperationSeries[] STRIPS(){
+
+
+    }
+
+
+
 }
