c85d1d61dc83f1b80c526608cc6040d200c9a268
seijiFeather <syag.feather@gmail.com>
2017-01-22 19:19:21 +0900

    メイン更新

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,47 +1,16 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import services.Application;
 import services.JSON.*;
-import services.naturalLanguage.*;
 import play.mvc.*;
 import views.html.*;
 
+import java.util.ArrayList;
 import java.io.IOException;
 
 import play.libs.Json;
 
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.annotation.JsonSetter;
-import java.util.*;
-
-class Response{
-	String id;
-	ArrayList<Integer> newPosition;
-	public Response(){
-		
-	}
-	@JsonSetter("id")
-	public void setId(String aId){
-		id = aId;
-	}
-	@JsonSetter("newPosition")
-	public void setnewPosition(int aX,int aY){
-		newPosition = new ArrayList<Integer>();
-		newPosition.add(aX);
-		newPosition.add(aY);
-	}
-	
-	public String getId(){
-		return(id);
-	}
-	
-	public ArrayList<Integer> getnewPosition(){
-		return(newPosition);
-	}
-}
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -59,28 +28,35 @@ public class HomeController extends Controller {
         System.out.println("############  run!!  ############");
 
         JsonNode json = request().body().asJson();
-        //String jsonString = json.toString();
         System.out.println(json);
-		ArrayList<Response> resList = new ArrayList<Response>();
-		Response res1 = new Response();
-		res1.setId("A");res1.setnewPosition(3,0);
-		resList.add(res1);
-		//System.out.println("List="+resList);
-		JsonNode resJson = Json.toJson(resList);
-		//System.out.println("Node="+resJson);
-		String resJ = Json.stringify(resJson);
-		//System.out.println("Res="+resJ);
-        //ObjectMapper mapper = new ObjectMapper();
-		//FieldData fd = Json.fromJson(json,FieldData.class);
-		//System.out.println("fd="+fd);
-        //Request tRequest = Json.fromJson(json, Request.class);
-		//System.out.println("tRequest="+tRequest);
-        //System.out.println(tRequest);
+        Request tRequest = Json.fromJson(json, Request.class);
+        Application tApp = new Application(tRequest);
+        ArrayList<OperationSeries> tResult = tApp.run();
+        JsonNode tResultJson = Json.toJson(tResult);
+        String tResultString = Json.stringify(tResultJson);
+        return ok(tResultString);
 
-        return ok(resJ);
+//        System.out.println(tRequest.getX());
+//        System.out.println(tRequest.getY());
+//        System.out.println(tRequest.getBlocks());
+//        System.out.println(tRequest.getOrder());
+//        ArrayList<String> tOrder = tRequest.getOrder();
+//        for (int i = 0; i < tOrder.size(); i++) {
+//            System.out.println(tOrder.get(i));
+//        }
+//
+//
+//        System.out.println(json);
+//        ArrayList<OperationSeries> resList = new ArrayList<OperationSeries>();
+//        OperationSeries res1 = new OperationSeries();
+//        res1.setId("A");
+//        res1.setnewPosition(3, 0);
+//        resList.add(res1);
+//        JsonNode resJson = Json.toJson(resList);
+//        String resJ = Json.stringify(resJson);
+//        return ok(resJ);
+
     }
 
 
 }
-
-
